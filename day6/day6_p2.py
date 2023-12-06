de89e20dadf4e48b2422b122c7a9d79d2e30f08b
Jan Conen <jan.conen@ifam.fraunhofer.de>
2023-12-06 15:39:36 +0100

    day6

---
+++ day6/day6_p2.py
@@ -0,0 +1,49 @@
+def get_text():
+    with open("day6/input.txt") as f:
+        return f.readlines()
+
+
+# distance = (time_total - time_charging) * 1 m * time_charging / s
+
+
+def calc_distance(total_time, charging_time):
+    return (total_time - charging_time) * charging_time
+
+
+def get_valid_runs(total_time, record_distance):
+    valid_runs = []
+    for charging_time in range(total_time):
+        distance = calc_distance(total_time, charging_time)
+        if distance > record_distance:
+            valid_runs.append(charging_time)
+        if valid_runs and distance <= record_distance:
+            break
+
+    return valid_runs
+
+
+def get_input_from_text(lines):
+    total_times = lines[0].rstrip().split()[1:]
+    record_distances = lines[1].rstrip().split()[1:]
+    total_time = int("".join(total_times))
+    record_distance = int("".join(record_distances))
+    return ([total_time], [record_distance])
+
+
+def main():
+    lines = get_text()
+    total_times, record_distances = get_input_from_text(lines)
+    possible_runs = []
+    for total_time, record_distance in zip(total_times, record_distances):
+        valid_runs = get_valid_runs(total_time, record_distance)
+        possible_runs.append(len(valid_runs))
+
+    combinations = 1
+    for run in possible_runs:
+        combinations *= run
+
+    print(combinations)
+
+
+if __name__ == "__main__":
+    main()
